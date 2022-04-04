d3bdfd51c584ac54b7d0f599aacf385b6b1c5990
Alex Grammatikopoulos <alexandrosgramma@gmail.com>
2022-04-04 09:56:36 +0300

    script change

--- src/Scenes/Lab5/Lab5.gd
+++ src/Scenes/Lab5/Lab5.gd
@@ -5,13 +5,13 @@ const MUSIC = preload("res://Assets/Music/lab_reset3.wav")
 
 func _ready() -> void:
 	Audio.play_music(MUSIC)
-	yield(get_tree().create_timer(0.1), "timeout")
-	Events.emit_signal("game_paused")
-	get_tree().paused = true
-	var dialog = Dialogic.start("LabLastOne")
-	dialog.pause_mode = Node.PAUSE_MODE_PROCESS
-	dialog.connect("timeline_end", self, "on_Dialog_timeline_end")
-	add_child(dialog)
+#	yield(get_tree().create_timer(0.1), "timeout")
+#	Events.emit_signal("game_paused")
+#	get_tree().paused = true
+#	var dialog = Dialogic.start("LabLastOne")
+#	dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+#	dialog.connect("timeline_end", self, "on_Dialog_timeline_end")
+#	add_child(dialog)
 
 
 func on_Dialog_timeline_end(timeline_name):
